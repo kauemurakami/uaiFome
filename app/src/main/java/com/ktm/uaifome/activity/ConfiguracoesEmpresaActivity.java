7c5d155ba6340ec15cf5edca3501e39b0d7f685d
kaue murakami <kauetmurakami@gmail.com>
2019-09-13 15:29:55 -0300

    Salvando dados da empresa

--- app/src/main/java/com/ktm/uaifome/activity/ConfiguracoesEmpresaActivity.java
+++ app/src/main/java/com/ktm/uaifome/activity/ConfiguracoesEmpresaActivity.java
@@ -1,23 +1,174 @@
 package com.ktm.uaifome.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.ktm.uaifome.R;
+import com.ktm.uaifome.helper.ConfiguracaoFirebase;
+import com.ktm.uaifome.helper.UsuarioFirebase;
+import com.ktm.uaifome.model.Empresa;
+
+import java.io.ByteArrayOutputStream;
 
 public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
+
+    private EditText edtNomeEmpresa, edtEmpresaCategoria, edtEmpresaTempo, edtEmpresaTaxa;
+    private ImageView imagemPerfilEmpresa;
+
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageReference;
+    private String idUsuarioLogado;
+    private String urlImagemSelecionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes_empresa);
 
+        //configurações iniciais
+        inicializarComponentes();
+        storageReference = ConfiguracaoFirebase.getFirebaseStorage();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
+
         //config toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //evento de click imagem para escolha da imagem
+        imagemPerfilEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        );
+                if (i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try {
+                switch (requestCode){
+                    case SELECAO_GALERIA:
+                        Uri localImagem = data.getData();
+                        imagem = MediaStore.Images
+                                .Media
+                                .getBitmap(
+                                        getContentResolver(),
+                                        localImagem
+                                );
+                        break;
+                }
+
+                if (imagem != null){
+                    imagemPerfilEmpresa.setImageBitmap(imagem);
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    StorageReference imagemRef = storageReference
+                            .child("imagens")
+                            .child("empresas")
+                            .child(idUsuarioLogado + "jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Erro ao fazer upload da imagem", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            urlImagemSelecionada = taskSnapshot.getDownloadUrl().toString();
+                            Toast.makeText(getApplicationContext(), "Imagem atribuida", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+            }catch (Exception e){
+
+            }
+        }
+    }
+
+    private void inicializarComponentes(){
+        edtEmpresaCategoria = findViewById(R.id.edtCategoriaEmpresa);
+        edtEmpresaTaxa      = findViewById(R.id.edtTaxaEntrega);
+        edtEmpresaTempo     = findViewById(R.id.edtTempoEntrega);
+        edtNomeEmpresa      = findViewById(R.id.edtNomeEmpresa);
+        imagemPerfilEmpresa = findViewById(R.id.profile_image);
+    }
+
+    //validação dos dados
+    public void validarDadosEmpresa(View v){
+        String nome, taxa, categoria, tempo;
+        nome = edtNomeEmpresa.getText().toString();
+        taxa = edtEmpresaTaxa.getText().toString();
+        categoria = edtEmpresaCategoria.getText().toString();
+        tempo = edtEmpresaTempo.getText().toString();
+
+        if (!nome.isEmpty()){
+            if (!categoria.isEmpty()){
+                if (taxa.isEmpty()) {
+                    taxa = String.valueOf(0);
+                }
+                if (tempo.isEmpty()){
+                    tempo = "Não definido";
+                }
+
+                Empresa empresa = new Empresa();
+                empresa.setIdUsuario(idUsuarioLogado);
+                empresa.setNome(nome);
+                empresa.setCategoria(categoria);
+                empresa.setPrecoEntrega(Double.parseDouble(taxa));
+                empresa.setTempo(tempo);
+                empresa.setUrlDaImagem(urlImagemSelecionada);
+                try {
+                    empresa.salvar();
+                    exibirMensagem("Salvo");
+                    finish();
+                }catch (Exception e){
+                    exibirMensagem("Falha ao salvar");
+                }
+            }else{
+                exibirMensagem("Descreva a categoria de sua empresa");
+            }
+        }else{
+            exibirMensagem("Digite um nome para sua empresa");
+        }
+
+
+    }
+
+    public void exibirMensagem(String texto){
+        Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/ktm/uaifome/model/Empresa.java
@@ -0,0 +1,69 @@
+package com.ktm.uaifome.model;
+
+import com.google.firebase.database.DatabaseReference;
+import com.ktm.uaifome.helper.ConfiguracaoFirebase;
+
+public class Empresa {
+
+    private String idUsuario, urlDaImagem, tempo, categoria, nome;
+    private Double  precoEntrega;
+
+    public Empresa() {
+
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUrlDaImagem() {
+        return urlDaImagem;
+    }
+
+    public void setUrlDaImagem(String urlDaImagem) {
+        this.urlDaImagem = urlDaImagem;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrecoEntrega() {
+        return precoEntrega;
+    }
+
+    public void setPrecoEntrega(Double precoEntrega) {
+        this.precoEntrega = precoEntrega;
+    }
+
+    public void salvar(){
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+        DatabaseReference empresaRef = firebaseRef.child("empresas")
+                .child(getIdUsuario());
+                empresaRef.setValue(this);
+    }
+}
